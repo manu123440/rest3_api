d1ca8ff6f7f946635971cabf9070c100f356bb1a
manu123440 <55475898+manu123440@users.noreply.github.com>
2023-09-10 17:43:26 +0530

    Update getVideo.js

--- routes/getVideo.js
+++ routes/getVideo.js
@@ -86,7 +86,7 @@ router.get('/video/:id',
 					      else {
 					       	let x = JSON.parse(response.body);
 
-					       	console.log(x);
+					       	// console.log(x);
 
 					       	if (x.length >= 1) {
 					       		const i = x[0].video_url;
@@ -124,7 +124,7 @@ router.get('/video/:id',
 												  else {
 												  	let z = JSON.parse(response.body);
 
-												  	console.log(z);
+												  	// console.log(z);
 
 												  	if (z.message === 'Success') {
 												  		return res.json({
